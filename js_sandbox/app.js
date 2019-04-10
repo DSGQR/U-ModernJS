eb8437d216dcafd1a88a09e83227df0522dd9256
DSGQR <amini.daniel@gmail.com>
2019-04-10 10:16:19 -0700

    Add JS Sandbox and review the console

---
+++ js_sandbox/app.js
@@ -0,0 +1,16 @@
+//  Log to console
+
+console.log('What are you doing?')
+console.log(123)
+console.log(true)
+var greeting = 'Hello'
+console.log(greeting)
+console.log([1,2,3,4,5])
+console.log({a:1, b:2})
+console.table({a:1, b:2})
+
+console.error('This is some error')
+console.clear()
+console.warn('This is a warning!')
+console.time('Hello')
+console.timeEnd('Hello')
