6eba8be2610e4325789991c463290c4f1900d703
DSGQR <amini.daniel@gmail.com>
2019-04-13 08:17:36 -0700

    Reviewed Local & Session Storage

---
+++ Local-Session-Storage/app.js
@@ -0,0 +1,50 @@
+// set local storage item
+// localStorage.setItem('name', 'John');
+// localStorage.setItem('age', '30');
+
+// set session storage item
+// sessionStorage.setItem('name', 'Beth');
+
+// remove from storage
+// localStorage.removeItem('name');
+// localStorage.removeItem('age');
+
+// get from storage
+// const name = localStorage.getItem('name');
+// const age = localStorage.getItem('age');
+
+// // clear local storage
+// localStorage.clear();
+
+// console.log(`NAME: ${name} | AGE: ${age}`);
+
+document.querySelector('form').addEventListener('submit', function(e){
+  const task = document.getElementById('task').value;
+
+  // localStorage.setItem('task', task)
+  // alert('Task Saved')
+
+  // console.log(task)
+
+  let tasks;
+
+  if(localStorage.getItem('tasks') === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+  }
+
+  tasks.push(task);
+
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+
+  alert('Task saved'); 
+
+  e.preventDefault();
+});
+
+const tasks = JSON.parse(localStorage.getItem('tasks'));
+
+tasks.forEach(function(task){
+  console.log(task);
+});
