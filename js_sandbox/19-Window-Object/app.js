dcb5e114925536c1872587841af9b6b962de00d9
DSGQR <amini.daniel@gmail.com>
2019-04-28 08:49:20 -0700

    Session 19 Completed

---
+++ js_sandbox/19-Window-Object/app.js
@@ -0,0 +1,59 @@
+// WINDOW METHODS / OBJECTS / PROPERTIES
+
+
+// Alert
+// window.alert('Hello World')
+
+// Prompt 
+// const input = prompt()
+// alert(input)
+
+// Confirm
+// if (confirm('Are You Sure?')) {
+//   console.log('YES')
+// } else {
+//   console.log('NO')
+// }
+
+let val
+
+// Outter Height & Width
+val = window.outerHeight
+val = window.outerWidth
+
+// Outter Height & Width
+val = window.innerHeight
+val = window.innerWidth
+
+// Scroll Points
+val = window.scrollY
+val = window.scrollX
+
+// Location Object
+val = window.location
+val = window.location.hostname
+val = window.location.port
+val = window.location.href
+val = window.location.search
+
+// Redirect
+// window.location.href = 'http://google.com'
+
+// Reload
+// window.location.reload()
+
+// HISTORY Object
+// window.history.go(-2)
+// val = window.history.length
+
+// Navigator Object
+val = window.navigator
+val = window.navigator.appName
+val = window.navigator.appVersion
+val = window.navigator.userAgent
+val = window.navigator.platform
+val = window.navigator.vendor
+val = window.navigator.language
+
+
+console.log(val)
