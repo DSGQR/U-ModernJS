8f50689c1cff2c883251578d1720d7a341e46349
DSGQR <amini.daniel@gmail.com>
2019-04-28 16:39:02 -0700

    Session 22 Completed

---
+++ DOM_Manipulation/21-WhatIsTheDOM/app.js
@@ -0,0 +1,5 @@
+// ---------------- DOCUMENT OBJECT MODEL ---------------- //
+
+// Tree of nodes / elements created by the browser
+// Javascript can be used to read/write/manipulate to the DOM
+// Object Oriented Respresentation 
