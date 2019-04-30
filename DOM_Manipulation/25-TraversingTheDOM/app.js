dd810013e1289a3a73cf1a3918abdf3004de040c
DSGQR <amini.daniel@gmail.com>
2019-04-30 06:06:59 -0700

    Session 25 In Progress...

---
+++ DOM_Manipulation/25-TraversingTheDOM/app.js
@@ -0,0 +1,32 @@
+let val
+
+const list = document.querySelector('ul.collection')
+const listItem = document.querySelector('li.collection-item:first-child')
+
+val = listItem
+val = list
+
+// Get Child Nodes
+val = list.childNodes
+val = list.childNodes[0]
+val = list.childNodes[0].nodeName
+val = list.childNodes[1].nodeType
+
+// 1 - Element
+// 2 - Attribute (deprecated)
+// 3 - Text node
+// 8 - Comment
+// 9 - Document Itself 
+// 10 - Doctype
+
+// Get children element nodes
+val = list.children
+val = list.children[1]
+list.children[1].textContent = 'Hello'
+// Children of children
+list.children[3].children[0].id = 'test-link'
+val = list.children[3].children[0]
+
+val = list.firstChild
+
+console.log(val)
